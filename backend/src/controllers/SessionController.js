08099e58518d5a4a43275cb4c16388bebc5c9da8
henrique <henriquecamposs@hotmail.com>
2020-03-26 08:56:21 -0300

    front and back

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,20 @@
+const connection = require('../database/connection');
+
+module.exports = {
+    
+    async create(request, response){
+        const { name, email, whatsapp, city, uf } = request.body;
+        const { id } = request.body;
+        console.log(id);
+        
+        const ong = await connection('ongs').where('id',id).select('name').first();
+
+    
+        if (!ong){
+           return response.status(400).json({ error: 'No ONG found with this ID'});
+        }
+
+        return response.json(ong);
+        
+    }
+}
